1d85b186fb804e896744e07819455981550ccfd1
levukk <levu120603@gmail.com>
2023-05-31 08:41:20 +0700

    fix sql loop nav

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -8,19 +8,16 @@ let getHome = (req, res) => {
   connection.query('SELECT * from `student`', function (error, results, fields) {
     if (error) {
       console.error(error);
-      connection.end();
       return res.status(500).send('Internal Server Error');
     }
 
     if (results) {
       data = results.map(row => {
-        console.log(row);
         return row;
       });
     }
 
-    connection.end();
-    return res.render('index.ejs', { dataUser: JSON.stringify(data) });
+    return res.render('index.ejs', { dataUser:data });
   });
 };
 
